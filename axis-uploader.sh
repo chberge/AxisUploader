6e976fd4f5a24203555e8db335352a5025cbb442
Chris-Håvard Berge <chb@oclin.no>
2018-06-29 19:10:07 +0200

    Create axis-uploader.sh

---
+++ axis-uploader.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Replace with your settings
+channel="NameOnSlackChannel"
+token="SlackBotToken"
+file="ImageFilename"
+url="http://ip.adress.here"
+size="1280x720"
+
+while true
+do
+
+# Downloading file from camera
+
+    wget "$url"/axis-cgi/jpg/image.cgi?resolution="$size" -O $file
+    
+
+# Uploading to Slack
+
+    curl -F file=@$file_a -F channels=#$channel -F token=$token https://slack.com/api/files.upload
+    
+# Wait 10 seconds
+    sleep 10
+    
+# Deleting file
+
+    rm $file
+
+# Waiting before running again (43200 (seconds) = 12 hours)
+
+    sleep 43200
+
+done
